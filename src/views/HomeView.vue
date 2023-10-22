13c1fe7eccedd498cb7c2489bdbaa8bc32b81821
Davino <davino.career@gmail.com>
2023-10-22 19:30:12 +0200

    Displaying data passed through a getCollection compsable on Home route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,27 @@
 <template>
 
-  <div class="home">Homepage</div>
+  <div class="home">
+    <div v-if="error" class="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id" >
+        {{ doc.title }}
+      </div>
+    </div>
+  </div>
 
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 
   export default {
-    name: 'HomeView',
+    name: 'Home',
+
+    setup(){
+      const { error, documents } = getCollection('blogpost');
+
+      return { error, documents}
+    }
   }
 </script>
 
